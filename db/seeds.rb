cf2487ba08fd5f1468eacfa315c7b9180ad1b1e5
happiguru <hguruman@gmail.com>
2021-11-07 17:27:49 +0400

    Update

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,12 @@ User.create(
     is_admin: true
 )
 
-# 8.times do
-#     House.create({
-#         name: Faker::Name.first_name,
-#         description: Faker::Lorem.sentence,
-#         price: Faker::Lorem.sentence,
-#         image: Faker::Lorem.sentence,
-#         user_id: 2
-#     })
-# end
+8.times do
+    House.create({
+        name: Faker::Name.first_name,
+        description: Faker::Lorem.sentence,
+        price: Faker::Lorem.sentence,
+        image: Faker::Lorem.sentence,
+        user_id: user.id
+    })
+end
